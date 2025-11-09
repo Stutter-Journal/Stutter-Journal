a888d419ac0ab1b49022685557210aae18244eb7
Milan Kosic <mimija.delfin@gmail.com>
2025-11-09 09:29:25 +0100

    feat: Add default configuration for multi modular builds

--- apps/mobile/build.gradle.kts
+++ apps/mobile/build.gradle.kts
@@ -1,7 +1,82 @@
+import com.android.build.gradle.AppExtension
+import com.android.build.gradle.AppPlugin
+import com.android.build.gradle.BaseExtension
+import com.android.build.gradle.LibraryExtension
+import com.android.build.gradle.LibraryPlugin
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.kotlinxSerialization) apply false
+}
+
+fun BaseExtension.defaultConfig() {
+    compileSdkVersion(35)
+
+    defaultConfig {
+        //noinspection OldTargetApi
+        targetSdk = 35
+        minSdk = 24
+    }
+
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_23
+        targetCompatibility = JavaVersion.VERSION_23
+    }
+}
+
+fun PluginContainer.applyDefaultConfig(project: Project) {
+    whenPluginAdded {
+        when (this) {
+            is AppPlugin -> {
+                project.extensions.getByType<AppExtension>().apply {
+                    defaultConfig()
+                }
+            }
+
+            is LibraryPlugin -> {
+                project.extensions.getByType<LibraryExtension>().apply {
+                    defaultConfig()
+                }
+            }
+
+            is JavaPlugin -> {
+                project.extensions.getByType<JavaPluginExtension>().apply {
+                    sourceCompatibility = JavaVersion.VERSION_23
+                    targetCompatibility = JavaVersion.VERSION_23
+                }
+            }
+        }
+    }
+}
+
+subprojects {
+    project.plugins.applyDefaultConfig(project)
+
+    tasks.withType<KotlinCompile>() {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_23)
+            freeCompilerArgs.addAll(
+                listOf(
+                    "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api",
+                )
+            )
+        }
+    }
 }

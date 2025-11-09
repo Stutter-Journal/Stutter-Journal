58e8ca26056b0d5244d6b034fe92fb774cada8a9
Milan Kosic <mimija.delfin@gmail.com>
2025-11-09 21:23:49 +0100

    feat(build): implement type-safe version catalog accessors for convention plugins

--- apps/mobile/build-logic/convention/src/main/kotlin/KmpLibraryConventionPlugin.kt
+++ apps/mobile/build-logic/convention/src/main/kotlin/KmpLibraryConventionPlugin.kt
@@ -3,7 +3,6 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
 class KmpLibraryConventionPlugin : Plugin<Project> {
--- apps/mobile/build-logic/settings.gradle.kts
+++ apps/mobile/build-logic/settings.gradle.kts
@@ -1,4 +1,5 @@
 @file:Suppress("UnstableApiUsage")
+
 rootProject.name = "build-logic"
 
 dependencyResolutionManagement {

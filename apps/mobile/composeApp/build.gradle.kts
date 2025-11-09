b8449b5516e85b272a113e7fdfb9e87c72681568
Milan Kosic <mimija.delfin@gmail.com>
2025-11-09 09:29:25 +0100

    feat: Add default configuration for multi modular builds

--- apps/mobile/composeApp/build.gradle.kts
+++ apps/mobile/composeApp/build.gradle.kts
@@ -34,9 +34,11 @@ kotlin {
             implementation(libs.androidx.activity.compose)
             implementation(libs.ktor.client.okhttp)
         }
+
         iosMain.dependencies {
             implementation(libs.ktor.client.darwin)
         }
+
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
@@ -62,29 +64,18 @@ kotlin {
 }
 
 android {
-    namespace = "com.jetbrains.kmpapp"
-    compileSdk = 35
+    namespace = "at.isg.eloquia"
+    compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.jetbrains.kmpapp"
-        minSdk = 24
+        applicationId = "at.isg.eloquia"
+
+        //noinspection OldTargetApi
         targetSdk = 35
+        minSdk = 24
+
         versionCode = 1
         versionName = "1.0"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_23
-        targetCompatibility = JavaVersion.VERSION_23
     }
 }
 

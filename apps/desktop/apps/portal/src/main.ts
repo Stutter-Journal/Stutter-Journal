d09767983ec253082ba068c909202298c0244ca1
Milan Kosic <e12121854@student.tuwien.ac.at>
2025-12-30 08:54:32 +0100

    refactor: Remove old application
    About to replace it with new template, to start off clean

--- apps/desktop/apps/portal/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { environment, resolveUseMocksFlag } from '@org/util';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
-
-environment.useMocks = resolveUseMocksFlag();
-
-bootstrapApplication(App, appConfig).catch((err) => console.error(err));

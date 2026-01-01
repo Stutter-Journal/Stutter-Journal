5f383d3a4d0672806460348438d3c02a6ac8401a
Milan Kosic <e12121854@student.tuwien.ac.at>
2026-01-01 11:55:53 +0100

    feat: Add entries libraries

---
+++ apps/desktop/libs/entries/feat-entry-list/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/feat-entry-list/feat-entry-list';
---
+++ apps/desktop/libs/entries/feat-entry-list/src/lib/feat-entry-list/feat-entry-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-feat-entry-list',
+  imports: [],
+  templateUrl: './feat-entry-list.html',
+  styleUrl: './feat-entry-list.css',
+})
+export class FeatEntryList {}

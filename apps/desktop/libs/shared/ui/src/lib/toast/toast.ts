fe89904c3f1fe0cb3c4d1da9fbac676a169fc38a
Milan Kosic <e12121854@student.tuwien.ac.at>
2026-01-03 16:25:03 +0100

    feat: Add toast as demo component

---
+++ apps/desktop/libs/shared/ui/src/lib/toast/toast.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-toast',
+  imports: [],
+  templateUrl: './toast.html',
+  styleUrl: './toast.scss',
+})
+export class Toast {
+
+}

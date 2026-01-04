31cd29630b9a351c0c3b0c13b016566f2592ad66
Milan Kosic <e12121854@student.tuwien.ac.at>
2026-01-04 21:28:54 +0100

    feat: Upgrade storybook migration

--- apps/desktop/apps/storybook-host/.storybook/main.ts
+++ apps/desktop/apps/storybook-host/.storybook/main.ts
@@ -1,3 +1,5 @@
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
 import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
@@ -8,7 +10,7 @@ const config: StorybookConfig = {
   ],
   addons: [],
   framework: {
-    name: '@storybook/angular',
+    name: getAbsolutePath("@storybook/angular"),
     options: {
       builder: {
         viteConfigPath: 'apps/storybook-host/vite.config.mts',
@@ -22,3 +24,7 @@ export default config;
 // To customize your Vite configuration you can use the viteFinal field.
 // Check https://storybook.js.org/docs/react/builders/vite#configuration
 // and https://nx.dev/recipes/storybook/custom-builder-configs
+
+function getAbsolutePath(value: string): any {
+  return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
+}

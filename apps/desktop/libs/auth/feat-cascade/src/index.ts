09b4809f10b61fe28cad43b0ac1b08524676dbdc
Milan Kosic <e12121854@student.tuwien.ac.at>
2026-01-05 15:14:36 +0100

    feat: Toying around with library creation and routing

---
+++ apps/desktop/libs/auth/feat-cascade/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/feat-cascade/feat-cascade';
---
+++ apps/desktop/libs/auth/feat-cascade/src/lib/feat-cascade/feat-cascade.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CascadeSelect } from 'primeng/cascadeselect';
+
+@Component({
+  selector: 'lib-feat-cascade',
+  imports: [CascadeSelect],
+  templateUrl: './feat-cascade.html',
+  styleUrl: './feat-cascade.css',
+})
+export class FeatCascade {}

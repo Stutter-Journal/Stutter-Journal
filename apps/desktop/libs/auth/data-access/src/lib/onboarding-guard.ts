432023e347b5e8f78a94edeeec5b4fe24344891c
Milan Kosic <e12121854@student.tuwien.ac.at>
2026-01-07 00:50:42 +0100

    feat: Add custom logging service

--- apps/desktop/libs/auth/data-access/src/lib/onboarding-guard.ts
+++ apps/desktop/libs/auth/data-access/src/lib/onboarding-guard.ts
@@ -1,10 +1,12 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { AuthClientService } from '@org/auth-data-access';
+import {LoggerService} from '@org/util';
 
 export const onboardingGuard: CanActivateFn = async () => {
   const auth = inject(AuthClientService);
   const router = inject(Router);
+  const log = inject(LoggerService);
 
   // If we already know the user, avoid the network call.
   let user = auth.user();
@@ -13,6 +15,8 @@ export const onboardingGuard: CanActivateFn = async () => {
   if (!user) {
     user = await auth.me();
   }
+
+  log.info(`User with username ${user?.displayName} has been fetched!`);
 
   // Not logged in -> go to landing
   if (!user) return router.parseUrl('/landing');
--- apps/desktop/libs/shared/util/src/lib/util/util.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-util',
-  imports: [],
-  templateUrl: './util.html',
-})
-export class Util {}

c4410487f821f13e504892661b8957c7f434b84c
Milan Kosic <e12121854@student.tuwien.ac.at>
2026-01-06 23:31:32 +0100

    refactor: Remove unused practice data-access component

--- apps/desktop/libs/practice/data-access/src/lib/practice-data-access/practice-data-access.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-practice-data-access',
-  imports: [],
-  templateUrl: './practice-data-access.html',
-  styleUrl: './practice-data-access.css',
-})
-export class PracticeDataAccess {}

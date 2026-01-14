71f6fb8bdcf47a6d2ef717b581d286a881119049
Milan Kosic <e12121854@student.tuwien.ac.at>
2026-01-14 14:17:34 +0100

    feat: Add loopback ip address for emulator debug builds

--- apps/mobile/composeApp/build.gradle.kts
+++ apps/mobile/composeApp/build.gradle.kts
@@ -7,13 +7,16 @@ android {
         buildConfig = true
     }
 
-    defaultConfig {
-        // TODO: Make this environment-specific when needed (debug/release can diverge).
-        buildConfigField("String", "BASE_URL", "\"http://api.eloquia.test/\"")
-    }
     buildTypes {
+        getByName("debug") {
+            // INFO: 10.0.2.2 is the hosts loopback interface, useful when working with the android emulator
+            buildConfigField("String", "BASE_URL", "\"http://10.0.2.2:8080/\"")
+        }
+
         getByName("release") {
             signingConfig = signingConfigs.getByName("debug")
+
+            buildConfigField("String", "BASE_URL", "\"http://api.eloquia.test/\"")
         }
     }
 }

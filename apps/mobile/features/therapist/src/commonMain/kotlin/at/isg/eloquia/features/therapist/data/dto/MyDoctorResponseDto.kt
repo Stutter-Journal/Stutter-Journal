a38de3195fa6c692b6c34196a0ea57410a2d727a
Milan Kosic <e12121854@student.tuwien.ac.at>
2026-01-14 20:32:21 +0100

    feat: Add DTOs

---
+++ apps/mobile/features/therapist/src/commonMain/kotlin/at/isg/eloquia/features/therapist/data/dto/MyDoctorResponseDto.kt
@@ -0,0 +1,10 @@
+package at.isg.eloquia.features.therapist.data.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MyDoctorResponseDto(
+    @SerialName("doctor") val doctor: DoctorDto
+)
+
---
+++ apps/mobile/features/therapist/src/commonMain/kotlin/at/isg/eloquia/features/therapist/data/dto/PracticeDto.kt
@@ -0,0 +1,9 @@
+package at.isg.eloquia.features.therapist.data.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class PracticeDto(
+    @SerialName("name") val name: String, @SerialName("address") val address: String? = null
+)

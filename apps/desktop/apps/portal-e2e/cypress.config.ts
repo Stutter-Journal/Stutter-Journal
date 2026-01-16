a6e9f4b48fd4c04cb26d9b252b47f355cc6124c0
Milan Kosic <e12121854@student.tuwien.ac.at>
2026-01-16 02:17:17 +0100

    refactor: Run formatter

--- apps/desktop/apps/portal-e2e/cypress.config.ts
+++ apps/desktop/apps/portal-e2e/cypress.config.ts
@@ -1,24 +1,24 @@
-import { nxE2EPreset } from "@nx/cypress/plugins/cypress-preset";
-import { defineConfig } from "cypress";
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:4200",
+    baseUrl: 'http://localhost:4200',
     ...nxE2EPreset(__filename, {
-      cypressDir: "cypress",
+      cypressDir: 'cypress',
       webServerCommands: {
-        default: "nx run portal:serve",
-        production: "nx run portal:serve:production",
+        default: 'nx run portal:serve',
+        production: 'nx run portal:serve:production',
       },
-      ciWebServerCommand: "nx run portal:serve-static",
+      ciWebServerCommand: 'nx run portal:serve-static',
     }),
   },
 
   component: {
     devServer: {
-      framework: "angular",
-      bundler: "webpack",
+      framework: 'angular',
+      bundler: 'webpack',
     },
-    specPattern: "**/*.cy.ts",
+    specPattern: '**/*.cy.ts',
   },
 });
